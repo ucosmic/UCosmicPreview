a8eec0f4933741ddcedca97b70f40aa498ecbf6c
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2012-04-12 14:55:19 -0400

    Add FindEstablishmentByEmail definition and handler for query processing.
    This is be needed by the sign on controller to determine email
    eligibility and sign on target.

---
+++ Apps/UCosmic.Domain/Establishments/FindEstablishmentByEmailHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace UCosmic.Domain.Establishments
+{
+    public class FindEstablishmentByEmailHandler : IHandleQueries<FindEstablishmentByEmailQuery, Establishment>
+    {
+        private readonly IQueryEntities _entities;
+
+        public FindEstablishmentByEmailHandler(IQueryEntities entities)
+        {
+            _entities = entities;
+        }
+
+        public Establishment Handle(FindEstablishmentByEmailQuery query)
+        {
+            return _entities.Establishments
+                .EagerLoad(query.EagerLoad, _entities)
+                .ByEmail(query.Email);
+        }
+    }
+}
---
+++ Apps/UCosmic.Domain/Establishments/FindEstablishmentByEmailQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace UCosmic.Domain.Establishments
+{
+    public class FindEstablishmentByEmailQuery : IDefineQuery<Establishment>
+    {
+        public string Email { get; set; }
+
+        public IEnumerable<Expression<Func<Establishment, object>>> EagerLoad { get; set; }
+    }
+}
