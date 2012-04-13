4106918aefa02f5446b739e0a20093930388b139
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2012-04-13 16:06:56 -0400

    Remove EstablishmentSamlFactory and encapsulate SamlSignOn metadata URL and EntityId updates into command.
    There is no longer a SetSamlSignOn method on the Establishment entity,
    or the need for a separate factory class. Seeders use the command instead.

---
+++ Apps/UCosmic.Domain/Establishments/UpdateSamlSignOnInfoCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace UCosmic.Domain.Establishments
+{
+    public class UpdateSamlSignOnInfoCommand
+    {
+        public Establishment Establishment { get; set; }
+        public string EntityId { get; set; }
+        public string MetadataUrl { get; set; }
+    }
+}
