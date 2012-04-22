bb2cd735279c638fc4ac6591714c30e8a9fdcc3c
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2012-04-22 13:35:02 -0400

    Add query + handler to get Person by Guid EntityId.
    This query will be needed to remove another action from SelfController.

---
+++ Apps/UCosmic.Domain/People/Queries/GetPersonByGuidQuery.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace UCosmic.Domain.People
+{
+    public class GetPersonByGuidQuery : BasePersonQuery, IDefineQuery<Person>
+    {
+        public Guid Guid { get; set; }
+    }
+}
