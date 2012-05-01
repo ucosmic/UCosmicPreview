3d84bad3905c02f53dba8a3c01d29b7944b14e28
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2012-04-30 23:40:52 -0400

    Add new Saml area.
    Metadata and integration controllers will go here.

---
+++ Apps/UCosmic.Www.Mvc/Areas/Saml/SamlAreaRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace UCosmic.Www.Mvc.Areas.Saml
+{
+    public class SamlAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get { return "saml"; }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            //context.MapRoute(
+            //    "Saml_default",
+            //    "Saml/{controller}/{action}/{id}",
+            //    new { action = "Index", id = UrlParameter.Optional }
+            //);
+        }
+    }
+}
