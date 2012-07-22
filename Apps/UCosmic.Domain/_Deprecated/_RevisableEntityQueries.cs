d9a22081e01ee5f1586a01516e0f53c2edc05cd4
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2012-07-22 18:57:20 -0400

    Remove RevisableEntityQueries methods to EntityExtensions class.

---
+++ Apps/UCosmic.Domain/_Deprecated/_RevisableEntityQueries.cs
@@ -0,0 +1,12 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using UCosmic.Domain;
+
+//namespace UCosmic
+//{
+//    public static class RevisableEntityQueries
+//    {
+//        // moved to entity extensions class
+//    }
+//}
